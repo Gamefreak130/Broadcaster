f98e1223b8f6c6f22062ec062548c61d9a38d41f
Gamefreak130 <9084643+Gamefreak130@users.noreply.github.com>
2020-01-30 10:24:24 -0600

    Rewrite listBoxMusic to display only safe file name while keeping entries unique

---
+++ Helpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gamefreak130.Broadcaster
+{
+    internal class MusicFile
+    {
+        internal string FullName { get; }
+
+        internal string DisplayName { get; }
+
+        internal MusicFile(string fullName, string displayName)
+        {
+            FullName = fullName;
+            DisplayName = displayName;
+        }
+
+        public override string ToString()
+        {
+            return DisplayName;
+        }
+    }
+}
