3c15bed8211e2d9465817c55e32ac5a66de3cb56
Gamefreak130 <jirvin4523@gmail.com>
2020-01-29 23:11:34 -0600

    Initial UI implementation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,8 @@
-﻿namespace Gamefreak130.Broadcaster
+﻿using System;
+
+namespace Gamefreak130.Broadcaster
 {
-    partial class Form1
+    partial class BroadcasterMain
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +31,173 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BroadcasterMain));
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.listBoxMusic = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.cmbStation = new System.Windows.Forms.ComboBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "MP3 files|*.mp3";
+            this.openFileDialog.Multiselect = true;
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog_FileOk);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Sims 3 Package|*.package";
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.SaveFileDialog_FileOk);
+            // 
+            // listBoxMusic
+            // 
+            this.listBoxMusic.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxMusic.FormattingEnabled = true;
+            this.listBoxMusic.ItemHeight = 18;
+            this.listBoxMusic.Location = new System.Drawing.Point(130, 14);
+            this.listBoxMusic.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBoxMusic.Name = "listBoxMusic";
+            this.listBoxMusic.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxMusic.Size = new System.Drawing.Size(343, 148);
+            this.listBoxMusic.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(130, 180);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(159, 42);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Music...";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(313, 180);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(159, 42);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove Selected";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.BtnRemove_Click);
+            // 
+            // cmbStation
+            // 
+            this.cmbStation.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbStation.FormattingEnabled = true;
+            this.cmbStation.Items.AddRange(new object[] {
+            "Beach Party",
+            "Chinese",
+            "Classical",
+            "Country",
+            "Dark Wave",
+            "Digitunes",
+            "Disco",
+            "Egyptian",
+            "Epic",
+            "French",
+            "Geek Rock",
+            "Hip Hop",
+            "Indie",
+            "Island Life",
+            "Kids",
+            "Latin",
+            "Pop",
+            "R&B",
+            "Rap",
+            "Rock",
+            "Rockabilly",
+            "Roots",
+            "Songwriter",
+            "Soul",
+            "Spooky",
+            "Western"});
+            this.cmbStation.Location = new System.Drawing.Point(129, 308);
+            this.cmbStation.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cmbStation.Name = "cmbStation";
+            this.cmbStation.Size = new System.Drawing.Size(343, 26);
+            this.cmbStation.Sorted = true;
+            this.cmbStation.TabIndex = 3;
+            this.cmbStation.TextChanged += new System.EventHandler(this.CmbStation_TextChanged);
+            this.cmbStation.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CmbStation_KeyPress);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.Transparent;
+            this.btnGenerate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnGenerate.Enabled = false;
+            this.btnGenerate.Font = new System.Drawing.Font("Trebuchet MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnGenerate.ImageKey = "Icon_sound_radio.jpg";
+            this.btnGenerate.ImageList = this.imageList;
+            this.btnGenerate.Location = new System.Drawing.Point(129, 358);
+            this.btnGenerate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(343, 68);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "   Broadcast";
+            this.btnGenerate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGenerate.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.White;
+            this.imageList.Images.SetKeyName(0, "Icon_sound_radio.jpg");
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(36, 258);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(557, 22);
+            this.lblInstructions.TabIndex = 5;
+            this.lblInstructions.Text = "Select an existing station to add music to, or type in a new station to create";
+            // 
+            // BroadcasterMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.cmbStation);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listBoxMusic);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "BroadcasterMain";
+            this.Text = "Broadcaster";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ListBox listBoxMusic;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox cmbStation;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.ImageList imageList;
     }
 }
 
